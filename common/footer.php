75a983f7d1ee7c61f69db8fedc776e5471cf370e
nhat21021997 <33777264+nhat21021997@users.noreply.github.com>
2018-06-12 15:18:10 +0700

    nhat

---
+++ common/footer.php
@@ -0,0 +1,11 @@
+<footer id="footer">
+	<section class="ft-top">
+		<div class="uk-flex uk-flex-middle uk-flex-space-between">
+			<a href="" title=""><img src="resources/img/icon/ft-a-1.png" alt=""></a>
+			<a href="" title=""><img src="resources/img/icon/ft-a-2.png" alt=""></a>
+			<a href="" title=""><img src="resources/img/icon/ft-a-3.png" alt=""></a>
+			<a href="" title=""><img src="resources/img/icon/ft-a-4.png" alt=""></a>
+			<a href="" title=""><img src="resources/img/icon/ft-a-5.png" alt=""></a>
+		</div>
+	</section><!-- ft-top -->
+</footer>
